ffae1d67dd329262050c4c852afd6e6df09a4b3a
mkhmmd17 <maxmelhi95@gmail.com>
2025-06-14 19:23:22 -0400

    implement Recent Sessions

--- app/page.tsx
+++ app/page.tsx
@@ -4,36 +4,27 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
 import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+
+
+    //? why in one we are using {limit} in second ()
+    const companions = await getAllCompanions({limit: 3});
+    const recentSessions = await getRecentSessions(10);
     return (
         <main>
             <h1>Popular Companions</h1>
             <section className="home-section">
-                <CompanionCard
-                    id="123"
-                    name="Neura the Brainy Explorer"
-                    topic="Neural Network of the Brain"
-                    subject="English Lecture"
-                    duration={45}
-                    color="#ffda6e"
-                />
-                <CompanionCard
-                    id="456"
-                    name="Countsy the Number Wizard"
-                    topic="Derivatives & Integrals"
-                    subject="Maths"
-                    duration={30}
-                    color="#e5d0ff"
-                />
-                <CompanionCard
-                    id="789"
-                    name="Verba the Vocabulary Builder"
-                    topic="language"
-                    subject="Science"
-                    duration={30}
-                    color="#bde7ff"
-                />
+                {companions?.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+
             </section>
             <section className="home-section">
                 <CompanionsList
